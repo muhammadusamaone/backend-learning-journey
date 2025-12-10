2ebf567bbeff60fd891155276a4d51aad31ddc79
Muhammad Usama <its.usamawahab@gmail.com>
2025-12-10 11:26:12 +0500

    Create grade.py

---
+++ Day3_Grade_Calculator/grade.py
@@ -0,0 +1,16 @@
+# Grade Calculator
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 80:
+    grade = "B"
+elif marks >= 70:
+    grade = "C"
+elif marks >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your Grade is:", grade)
